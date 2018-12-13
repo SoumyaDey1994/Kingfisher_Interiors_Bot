76c4a3a4c9063304d7f53318baac6073a230efae
Soumya Kanta Dey <soumyakanta.dey@itcinfotech.com>
2018-12-13 16:18:30 +0530

    Kingfisher-Interiors-Training-Assistant-Bot

---
+++ Kingfisher PLC documents/External_User_Document/config.js
@@ -0,0 +1,177 @@
+IDRViewer.config = {
+    "pagecount":43,
+    "title":"Training Manual - Brooks Sports",
+    "author":"Harshit Nigam",
+    "fileName":"Training_Manual_Vendor_Portal_v3.pdf",
+    "bounds":[
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286],
+        [909,1286],[909,1286],[909,1286],[909,1286]
+    ],
+        "bookmarks":[
+            {
+                "title":"1.0 Activities of a Vendor/Finished Goods Factory in flexplm",
+                "page":3,
+                "zoom":"XYZ 110 1154 0"
+            }, 
+            {
+                     "title":"1.1 How To Log -In To Flex PL M",
+                     "page":3,
+                     "zoom":"XYZ 110 775 0",
+                    "children":[
+                        {
+                            "title":"1.1.1 Procedure or Logging into FlexPLM",
+                            "page":3,
+                            "zoom":"XYZ 110 623 0"
+                        }
+                    ]
+            },
+            {
+                    "title":"1.2 BOM",
+                    "page":4,"zoom":"XYZ 110 834 0",
+                    "children":[
+                        {
+                            "title":"1.1.1 Create BOM",
+                            "page":4,"zoom":"XYZ 110 699 0",
+                            "children":[
+                                {
+                                    "title":"1.1.1.1 Create BOM from Season Dashboard",
+                                    "page":4,
+                                    "zoom":"XYZ 110 480 0"
+                                },
+                                {
+                                    "title":"1.1.1.2 Add BOM Components",
+                                    "page":8,
+                                    "zoom":"XYZ 110 405 0"
+                                },
+                                {
+                                    "title":"1.1.1.3 Adding Colors to the Material Components",
+                                    "page":10,
+                                    "zoom":"XYZ 110 334 0"
+                                },
+                                {
+                                    "title":"1.1.1.4 Completing a BOM line item to get the Total Price",
+                                    "page":12,
+                                    "zoom":"XYZ 110 536 0"
+                                },
+                                {
+                                    "title":"1.1.1.5 Remove a Material from the BOM",
+                                    "page":14,
+                                    "zoom":"XYZ 110 674 0"
+                                },
+                                {
+                                    "title":"1.1.1.6 Smart Insert Material",
+                                    "page":15,
+                                    "zoom":"XYZ 110 962 0"
+                                },
+                                {
+                                    "title":"1.1.1.7 Create BoM with Colorway Variation",
+                                    "page":16,
+                                    "zoom":"XYZ 110 834 0"
+                                },
+                                {
+                                    "title":"1.1.1.8 Create BoM from Product Library",
+                                    "page":18,
+                                    "zoom":"XYZ 110 490 0"
+                                },
+                                {
+                                    "title":"1.1.1.9 Update BOM",
+                                    "page":19,
+                                    "zoom":"XYZ 110 566 0"
+                                },
+                                {
+                                    "title":"1.1.1.10 Copy & Paste BOM",
+                                    "page":21,
+                                    "zoom":"XYZ 110 488 0"
+                                },
+                                {
+                                    "title":"1.1.1.11 Export BOM in excel or in pdf",
+                                    "page":25,
+                                    "zoom":"XYZ 110 1191 0"
+                                }
+                            ]}
+                        ]
+            },
+            {
+                        "title":"1.3 Cost Sheet",
+                        "page":29,
+                        "zoom":"XYZ 110 1191 0",
+                        "children":[
+                                        {
+                                            "title":"1.3.1 Update Cost Sheet",
+                                            "page":29,
+                                            "zoom":"XYZ 110 1151 0",
+                                            "children":[
+                                                        {
+                                                            "title":"1.3.1.1 Navigate to Cost Sheet from Season Dashboard",
+                                                            "page":29,
+                                                            "zoom":"XYZ 110 788 0"
+                                                        },
+                                                        {
+                                                            "title":"1.3.1.2 Navigate to Cost Sheet from Product Library",
+                                                            "page":33,
+                                                            "zoom":"XYZ 110 1191 0"
+                                                        }
+                                                    ]
+                                        }
+                                    ]
+            },
+            {
+                        "title":"1.4 Generate Tech Pack",
+                        "page":34,
+                        "zoom":"XYZ 110 1191 0",
+                        "children":[
+                                        {
+                                            "title":"1.4.1 Generate Tech Pack from Season Dashboard",
+                                            "page":34,
+                                            "zoom":"XYZ 110 919 0"
+                                        },
+                                        {
+                                            "title":"1.4.2 Generate Tech Pack from Product Library",
+                                            "page":38,
+                                            "zoom":"XYZ 110 1191 0"
+                                        },
+                                        {
+                                            "title":"1.4.3 Download Tech Pack from Document Library",
+                                            "page":39,
+                                            "zoom":"XYZ 110 1191 0"
+                                        }
+                                    ]
+            },
+            {
+                        "title":"1.5 View Material",
+                        "page":40,
+                        "zoom":"XYZ 110 1191 0"
+            },
+            {
+                            "title":"1.6 View Color",
+                            "page":41,
+                            "zoom":"XYZ 110 566 0"
+            },
+            {
+                            "title":"1.7 View Document",
+                            "page":42,
+                            "zoom":"XYZ 110 703 0"
+            }
+        
+        ],
+        "thumbnailType":"jpg",
+        "pageType":"html",
+        "pageLabels":[]
+};
